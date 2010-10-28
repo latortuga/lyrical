65a57092760860a60579862c0fac4481467b7cca
Drew Ulmer <latortuga@gmail.com>
2010-10-27 22:09:53 -0500

    Renamed some variables to avoid overwriting a parameter

--- lib/lyrical/lyrics_finder.rb
+++ lib/lyrical/lyrics_finder.rb
@@ -1,9 +1,8 @@
 module Lyrical
   class LyricsFinder
     def self.find(query)
-      # replace spaces with pluses, lowercase it
-      query.gsub!(/\s/, '+').downcase!
-      url = GoogleUrl.gsub('?query', query)
+      finder = query.gsub(/\s/, '+').downcase
+      url = GoogleUrl.gsub('?query', finder)
       # Search for nodes by css
       Hpricot(open(url)).search('h3.r a.l').map {|link| link.inner_html}
     end
