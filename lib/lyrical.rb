bdc4ee0faf8ab7643603caf5a2b3848996e92a6c
Drew Ulmer <latortuga@gmail.com>
2010-10-27 22:06:26 -0500

    Added constants and includes to the base lib.

--- lib/lyrical.rb
+++ lib/lyrical.rb
@@ -1,3 +1,10 @@
 require 'lyrical/lyric'
+require 'lyrical/lyrics_finder'
+require 'rubygems'
+require 'hpricot'
+require 'open-uri'
+
 module Lyrical
+  LyricsSites = ['azlyrics.com', 'elyrics.net', 'musicsonglyrics.com']
+  GoogleUrl = "http://www.google.com/search?q=?query+song+lyrics"
 end
